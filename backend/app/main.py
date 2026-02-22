c91c92b762e629f7b4695afc7dbab3dc25319e2d
Sahil Seemant <139580558+limitless235@users.noreply.github.com>
2026-02-23 00:51:46 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,  # Must be False when allow_origins is ["*"]
     allow_methods=["*"],
     allow_headers=["*"],
 )
