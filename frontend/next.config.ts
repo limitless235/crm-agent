df912397a5d4ec11b5c8983b34164a6567b00e57
Sahil Seemant <139580558+limitless235@users.noreply.github.com>
2026-02-24 01:04:26 +0530

    COOP Final Fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
